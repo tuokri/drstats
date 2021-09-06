d2b149f202e9817acf3914435cb4a1fc093d7a14
Tuomo <tuokri@tuta.io>
2021-09-07 00:20:04 +0300

    Add explicit ident column

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class Statistic(db.Model):
     __tablename__ = "Statistics"
 
     # server_addr = Column(String(15))
+    ident = db.Column("ident", db.Sequence(start=1, increment=1))
     date = db.Column("date", db.DateTime)
     stats = db.Column("stats", db.String)
 
